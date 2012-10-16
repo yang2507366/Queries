93aefa6b343fd1efe52d8b4479218798e9511f3f
yangzexin <yang380065007@hotmail.com>
2012-10-16 20:36:10 +0800

    Initial Commit

---
+++ Queries/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Queries
+//
+//  Created by yangzexin on 10/16/12.
+//  Copyright (c) 2012 yangzexin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
