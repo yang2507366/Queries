f2ae28cfb909ae82ec64eeecab2c74a4514bce62
yangzexin <yang380065007@hotmail.com>
2012-10-21 21:53:04 +0800

    c

---
+++ Queries/CallScriptImpl.h
@@ -0,0 +1,13 @@
+//
+//  CallScriptImpl.h
+//  Queries
+//
+//  Created by yangzexin on 10/21/12.
+//  Copyright (c) 2012 yangzexin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CallScriptImpl : NSObject
+
+@end
---
+++ Queries/CallScriptImpl.m
@@ -0,0 +1,13 @@
+//
+//  CallScriptImpl.m
+//  Queries
+//
+//  Created by yangzexin on 10/21/12.
+//  Copyright (c) 2012 yangzexin. All rights reserved.
+//
+
+#import "CallScriptImpl.h"
+
+@implementation CallScriptImpl
+
+@end
