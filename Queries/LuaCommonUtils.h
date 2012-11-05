3f1271569d6498b2c2736377f50232a8bfe89059
yangzexin <yang380065007@hotmail.com>
2012-11-05 09:50:39 +0800

    c

---
+++ Queries/LuaCommonUtils.h
@@ -0,0 +1,13 @@
+//
+//  LuaCommonUtils.h
+//  Queries
+//
+//  Created by yangzexin on 12-11-5.
+//  Copyright (c) 2012年 yangzexin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LuaCommonUtils : NSObject
+
+@end
---
+++ Queries/LuaCommonUtils.m
@@ -0,0 +1,13 @@
+//
+//  LuaCommonUtils.m
+//  Queries
+//
+//  Created by yangzexin on 12-11-5.
+//  Copyright (c) 2012年 yangzexin. All rights reserved.
+//
+
+#import "LuaCommonUtils.h"
+
+@implementation LuaCommonUtils
+
+@end
