17b343186ec52b923bb62bd3ec3397bc88467852
yangzexin <yang380065007@hotmail.com>
2012-11-12 23:00:59 +0800

    c

--- Queries/LuaScriptInteraction.m
+++ Queries/LuaScriptInteraction.m
@@ -16,34 +16,6 @@
 #import "NSString+Substring.h"
 #import "LuaConstants.h"
 
-@interface LuaFuncReturnValue : NSObject
-
-+ (id)createWithReturnValue:(NSString *)returnValue error:(NSString *)error;
-@property(nonatomic, copy)NSString *returnValue;
-@property(nonatomic, copy)NSString *error;
-
-@end
-
-@implementation LuaFuncReturnValue
-
-- (void)dealloc
-{
-    self.returnValue = nil;
-    self.error = nil;
-    [super dealloc];
-}
-
-+ (id)createWithReturnValue:(NSString *)returnValue error:(NSString *)error
-{
-    LuaFuncReturnValue *tmp = [[LuaFuncReturnValue new] autorelease];
-    tmp.returnValue = returnValue;
-    tmp.error = error;
-    
-    return tmp;
-}
-
-@end
-
 @interface LuaScriptInteraction () {
     char *_scriptChars;
     lua_State *_L;
@@ -88,47 +60,6 @@ int get_module(lua_State *L)
     return 1;
 }
 
-char *lua_typeof(int index)
-{
-    switch (index)
-    {
-        case LUA_TNIL:            return "nil";
-        case LUA_TNUMBER:         return "number";
-        case LUA_TBOOLEAN:        return "boolean";
-        case LUA_TSTRING:         return "string";
-        case LUA_TTABLE:          return "table";
-        case LUA_TFUNCTION:       return "function";
-        case LUA_TUSERDATA:       return "userdata";
-        case LUA_TTHREAD:         return "thread";
-        case LUA_TLIGHTUSERDATA:  return "light userdata";
-            
-        default: return "unknown type";
-    }
-}
-
-int HAPI_echo(lua_State* L)
-{
-    int args_from_lua = lua_gettop(L);
-    
-    //in this example, we'll take any number of args
-    printf("HAPI_echo() called with %d arguments\n",
-           args_from_lua);
-    for (int n = 1; n <= args_from_lua; ++n)
-    {
-        printf(" * arg %02d (%s):\t%s\n",
-               n,
-               lua_typeof(lua_type(L, n)),
-               lua_tostring(L, n));
-        //note: lua_tostring coerces stack value!
-    }
-    
-    //return (123, "abc") to lua
-    lua_pushnumber(L, 123);
-    lua_pushstring(L, "abc");
-    return 2; // 2 return values are on the stack
-    
-}
-
 - (id)initWithScript:(NSString *)script
 {
     self = [super init];
@@ -148,6 +79,11 @@ int HAPI_echo(lua_State* L)
     return self;
 }
 
+void attachCFunctions(lua_State *L)
+{
+    initFuntions(L);
+}
+
 - (void)callFunction:(NSString *)funcName callback:(void(^)(NSString *returnValue, NSString *error))callback parameters:(NSString *)firstParameter,...
 {
     if(!_scriptChars){
@@ -164,7 +100,7 @@ int HAPI_echo(lua_State* L)
     
     lua_getglobal(_L, [funcName UTF8String]);
     
-    initFuntions(_L);
+    attachCFunctions(_L);
     
     NSInteger parameterCount = 0;
     va_list args;
@@ -209,7 +145,7 @@ int HAPI_echo(lua_State* L)
             errorMsg = @"lua errerr";
         }
         errorMsg = [NSString stringWithFormat:@"%@\n%s", errorMsg, lua_tostring(_L, -1)];
-        NSLog(@"callFunction:%@, error:%@", funcName, errorMsg);
+        NSLog(@"Function:%@, error:%@", funcName, errorMsg);
     }
     if(callback){
         callback(nil, errorMsg);
@@ -230,7 +166,7 @@ int HAPI_echo(lua_State* L)
     
     lua_getglobal(_L, [funcName UTF8String]);
     
-    initFuntions(_L);
+    attachCFunctions(_L);
     
     NSInteger parameterCount = 0;
     va_list args;
@@ -272,61 +208,10 @@ int HAPI_echo(lua_State* L)
             errorMsg = @"lua errerr";
         }
         errorMsg = [NSString stringWithFormat:@"%@\n%s", errorMsg, lua_tostring(_L, -1)];
-        NSLog(@"callFunction:%@, error:%@", funcName, errorMsg);
+        NSLog(@"Function:%@, error:%@", funcName, errorMsg);
     }
     lua_pop(_L, 1);
     return @"";
-}
-
-- (LuaFuncReturnValue *)callWithFunctionName:(NSString *)funcName parameters:(NSString *)firstParameter, ...
-{
-    if(!_scriptChars){
-        return nil;
-    }
-    
-    lua_getglobal(_L, [funcName UTF8String]);
-    
-    initFuntions(_L);
-    
-    NSInteger parameterCount = 0;
-    va_list args;
-    
-    if(firstParameter){
-        va_start(args, firstParameter);
-        for(NSString *tmpParameter = firstParameter; tmpParameter; tmpParameter = va_arg(args, id), ++parameterCount){
-            if(self.scriptInvokeFilter){
-                tmpParameter = [self.scriptInvokeFilter filterParameter:tmpParameter];
-            }
-            lua_pushstring(_L, [tmpParameter UTF8String]);
-        }
-        va_end(args);
-    }
-    
-    int result = lua_pcall(_L, parameterCount, 1, 0);
-    NSString *errorMsg = @"unknown error";
-    if(result == 0){
-        const char *returnValue = lua_tostring(_L, -1);
-        NSString *returnString = @"";
-        if(returnValue){
-            returnString = [NSString stringWithUTF8String:returnValue];
-        }
-        if(self.scriptInvokeFilter){
-            returnString = [self.scriptInvokeFilter filterReturnValue:returnString];
-        }
-        return [LuaFuncReturnValue createWithReturnValue:returnString error:nil];
-    }else if(result == LUA_YIELD){
-        errorMsg = @"lua yield";
-    }else if(result == LUA_ERRRUN){
-        errorMsg = @"lua errrun";
-    }else if(result == LUA_ERRSYNTAX){
-        errorMsg = @"lua errsyntax";
-    }else if(result == LUA_ERRMEM){
-        errorMsg = @"lua errmem";
-    }else if(result == LUA_ERRERR){
-        errorMsg = @"lua errerr";
-    }
-    D_Log(@"callFunction:%@, error:%@", funcName, errorMsg);
-    return nil;
 }
 
 - (void)setScript:(NSString *)pScript
