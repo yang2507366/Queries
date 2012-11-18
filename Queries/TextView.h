3a3834c4217c3109505aadca1790ed85e450ef04
yangzexin <yang380065007@hotmail.com>
2012-11-18 22:21:45 +0800

    c

---
+++ Queries/TextView.h
@@ -0,0 +1,13 @@
+//
+//  TextView.h
+//  Queries
+//
+//  Created by yangzexin on 11/18/12.
+//  Copyright (c) 2012 yangzexin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TextView : NSObject
+
+@end
---
+++ Queries/TextView.m
@@ -0,0 +1,13 @@
+//
+//  TextView.m
+//  Queries
+//
+//  Created by yangzexin on 11/18/12.
+//  Copyright (c) 2012 yangzexin. All rights reserved.
+//
+
+#import "TextView.h"
+
+@implementation TextView
+
+@end
