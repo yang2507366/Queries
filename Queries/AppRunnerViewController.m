98937e1e991fbd5800f096976cce56425a03c4ac
yangzexin <yang380065007@hotmail.com>
2012-11-24 18:11:42 +0800

    c

--- Queries/AppRunnerViewController.m
+++ Queries/AppRunnerViewController.m
@@ -85,7 +85,7 @@
     [loader loadWithCompletion:^(NSString *filePath) {
         [Waiting showWaiting:NO inView:self.view];
         if(filePath.length == 0){
-            [DialogTools showWithTitle:@"" message:@"加载失败，请检查输入的URL地址" completion:nil cancelButtonTitle:@"确定" otherButtonTitleList:nil];
+            [DialogTools showWithTitle:@"" message:@"加载失败，请检查URL地址" completion:nil cancelButtonTitle:@"确定" otherButtonTitleList:nil];
             return;
         }
         ZipArchive *zipAr = [[[ZipArchive alloc] init] autorelease];
