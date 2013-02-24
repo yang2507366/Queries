d2f00ca447c565d4062610f505429897d30d1d86
yangzexin <yang380065007@hotmail.com>
2013-02-24 14:09:46 +0800

    c

--- Resources/BaseScripts.bundle/UIKit/UIViewController.lua
+++ Resources/BaseScripts.bundle/UIKit/UIViewController.lua
@@ -4,10 +4,11 @@ require "UINavigationItem"
 require "AppContext"
 require "CommonUtils"
 require "NSArray"
+require "UINavigationBar"
+require "UIToolbar"
 
-UIViewController = {};
-UIViewController.__index = UIViewController;
-setmetatable(UIViewController, Object);
+class(UIViewController);
+class(UINavigationController);
 
 -- constructor
 function UIViewController:create(title)
@@ -278,5 +279,56 @@ function UIViewController_viewDidPop(vcId)
     end
 end
 
+--[[ ------------------------------------------- ]]
+-- constructor
+function UINavigationController:create(rootVc--[[option]])
+    if rootVc ~= nil then
+        local ncId = runtime::invokeClassMethod("LINavigationController", "create", AppContext.current());
+        local nc = self:get(ncId);
+        
+        nc:pushViewController(rootVc, false);
+        
+        return nc;
+    end
+    return nil;
+end
 
+function UINavigationController:get(ncId)
+    local nc = UIViewController:new(ncId);
+    setmetatable(nc, self);
+    
+    return nc;
+end
+
+-- instance methods
+function UINavigationController:pushViewController(vc, animated--[[option]])
+    if animated == nil then
+        animated = true;
+    end
+    runtime::invokeMethod(self:id(), "pushViewController:animated:", vc:id(), toObjCBool(animated));
+end
+
+function UINavigationController:toolbarHidden()
+    return toLuaBool(runtime::invokeMethod(self:id(), "toolbarHidden"));
+end
+
+function UINavigationController:setToolbarHidden(hidden, animated)
+    runtime::invokeMethod(self:id(), "setToolbarHidden:animated:", toObjCBool(hidden), toObjCBool(animated));
+end
+
+function UINavigationController:navigationBarHidden()
+    return toLuaBool(runtime::invokeMethod(self:id(), "navigationBarHidden"));
+end
+
+function UINavigationController:setNavigationBarHidden(hidden, animated)
+    runtime::invokeMethod(self:id(), "setNavigationBarHidden:animated", toObjCBool(hidden), toObjCBool(animated));
+end
+
+function UINavigationController:navigationBar()
+    return UINavigationBar:get(runtime::invokeMethod(self:id(), "navigationBar"));
+end
+
+function UINavigationController:toolbar()
+    return UIToolbar:get(runtime::invokeMethod(self:id(), "toolbar"));
+end
 
